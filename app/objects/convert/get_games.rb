8cc88d6cf125f291b0e0a626f6712e87959d36fd
Steve Alex <salex@mac.com>
2024-04-30 12:04:12 -0500

    reset gitignore files

--- app/objects/convert/get_games.rb
+++
@@ -1,9 +0,0 @@
-module Convert
-  class GetGames
-    attr_accessor :games, :json
-    def initialize
-      @json = File.read(Rails.root.join('app','objects','convert','set_games.json'))
-      @games = JSON.parse(@json)
-    end
-  end
-end
--- app/objects/convert/get_rounds.rb
+++
@@ -1,9 +0,0 @@
-module Convert
-  class GetRounds
-    attr_accessor :rounds
-    def initialize
-      json = File.read(Rails.root.join('app','objects','convert','set_rounds.json'))
-      @rounds = JSON.parse(json)
-    end
-  end
-end
